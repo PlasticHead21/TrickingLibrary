c635b65d63419ebc5e7723a4ccb5d4060cd54ea9
dmartynenka@gloriumtech.com <dmartynenka@gloriumtech.com>
2021-11-25 00:15:32 +0300

    init

---
+++ TrickingLibrary.Api/Startup.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrickingLibrary.Api
+{
+    public class Startup
+    {
+        private const string ALLCORS = "All";
+        
+        public Startup(IConfiguration configuration)
+        {
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddCors(options => options.AddPolicy(ALLCORS, builder =>
+                builder.AllowAnyHeader()
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseCors(ALLCORS);
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+        }
+    }
+}
